c5ee6cdebd5759859b4845128b52f9c9acbe13a2
Matthew Bower <mttbwr91@gmail.com>
2022-03-04 22:38:51 -0800

    mern images loading

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
   return (
     <div className="card-container">
-      <img alt="" src={`/data.json/${props.image}`} />
+      <img alt="" src={`${props.image}`} />
       {/* monster comes off of props because monster is a prop in CardList, where Card is imported */}
       <h2>{props.name}</h2>
       {/* <p>{props.email}</p> */}
