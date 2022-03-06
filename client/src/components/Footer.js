877ad7894b8638f55318cd12d7996271e512a9bd
Matthew Bower <mttbwr91@gmail.com>
2022-03-06 15:12:31 -0800

    more mernName logos

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,10 +3,17 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-header"></div>
-      <div className="footer-child">This is row 1 of the footer</div>
-      <div className="footer-child">This is row 2 of the footer</div>
-      <div className="footer-child">Github, Linkedin, Youtube, Instagram</div>
+      <div className="footer-header">Thanks! Get in touch with me.</div>
+      <div className="footer-child">row 1</div>
+      <div className="footer-child">
+        <div className="span">row 2</div>
+      </div>
+      <div className="footer-child">
+        <div className="instagram">Github</div>
+        <div className="linkedin">Linkedin</div>
+        <div className="youtube">Youtube</div>
+        <div className="twitch">Twitch</div>
+      </div>
     </div>
   );
 };
