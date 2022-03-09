9ff0172d4b9851839cb92ea77ff3bb277ed1edd5
Matthew Bower <mttbwr91@gmail.com>
2022-03-08 20:08:26 -0800

    footer looks good

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,17 +2,58 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-header">Thanks! Get in touch with me.</div>
-      <div className="footer-child">row 1</div>
-      <div className="footer-child">
-        <div className="span">row 2</div>
-      </div>
-      <div className="footer-child">
-        <div className="instagram">Github</div>
-        <div className="linkedin">Linkedin</div>
-        <div className="youtube">Youtube</div>
-        <div className="twitch">Twitch</div>
+    <div class="footer">
+      <div className="container">
+        <div className="row">
+          <div className="footer-col">
+            <h4>Services</h4>
+            <ul>
+              <li>
+                <a href="#">MB Tutorials</a>
+              </li>
+              <li>
+                <a href="#">Fiverr</a>
+              </li>
+              <li>
+                <a href="#">Craigslist</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Socials</h4>
+            <ul>
+              <li>
+                <a href="#">
+                  <i className="fab fa-github"></i> Github
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fab fa-linkedin"></i> Linkedin
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fab fa-youtube"></i> Youtube
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fab fa-twitter"></i> Twitter
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="fab fa-twitch"></i> Twitch
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Subscribe</h4>
+            <form>form</form>
+          </div>
+        </div>
       </div>
     </div>
   );
